6c2434751016d4207ae548a96706d08397f3c3d4
Mogili Deepika <mogilideepika218@gmail.com>
2025-04-13 18:46:55 +0530

    removed comments

--- backend/pkg/websocket/websocket.go
+++ backend/pkg/websocket/websocket.go
@@ -14,24 +14,6 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
-// func Reader(conn *websocket.Conn) {
-// 	for {
-// 		messageType, p, err := conn.ReadMessage()
-
-// 		if err != nil {
-// 			log.Println(err)
-// 			return
-// 		}
-
-// 		fmt.Println("msg", string(p))
-
-// 		if err := conn.WriteMessage(messageType, p); err != nil {
-// 			log.Println(err)
-// 			return
-// 		}
-// 	}
-// }
-
 func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 	fmt.Println(r.Host)
 
@@ -44,31 +26,3 @@ func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 
 	return conn, nil
 }
-
-// func Writer(conn *websocket.Conn) {
-// 	fmt.Println("message sending .......")
-
-// 	messageType, r, err := conn.NextReader()
-
-// 	if err != nil {
-// 		log.Println(err)
-// 		return
-// 	}
-
-// 	w, err := conn.NextWriter(messageType)
-
-// 	if err != nil {
-// 		log.Println(err)
-// 		return
-// 	}
-
-// 	if _, err := io.Copy(w, r); err != nil {
-// 		fmt.Println(err)
-// 		return
-// 	}
-
-// 	if err := w.Close(); err != nil {
-// 		fmt.Println(err)
-// 		return
-// 	}
-// }
